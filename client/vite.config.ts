72ab7def2dabb39cda94dedc4b68fbc4078d022e
ffrub <ff.rueb@gmail.com>
2023-04-17 15:28:20 +0200

    build(deps): update deps to fix build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(['aws-appsync'])],
+    },
+  },
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
